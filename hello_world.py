e9daf89ead25176bf9247fce3c77b00f33477d55
Pavla Moravkova <pavla.moravkova@datasentics.com>
2021-03-04 11:26:46 +0100

    snad uz to pobezi

--- hello_world.py
+++ hello_world.py
@@ -8,11 +8,11 @@ data_folder = Path('./data')
 #source_file_path = Path('data/in/tables/source.csv')
 #result_file_path = Path('data/out/tables/output.csv')
 
-source_file_path = data_folder.joinpath('in/tables/source.csv')
+source_file_path = data_folder.joinpath('in/tables/input.csv')
 result_file_path = data_folder.joinpath('out/tables/output.csv')
 
 config = json.load(open(data_folder.joinpath('config.json')))
-param_print_lines = config['parameters']['print_rows']
+#param_print_lines = config['parameters']['print_rows']
 
 print('Running')
 
@@ -24,7 +24,8 @@ with open(source_file_path, 'r') as input, open(result_file_path, 'w+', newline=
     writer = csv.DictWriter(out, fieldnames=new_columns, lineterminator='\n', delimiter = ',')
     writer.writeheader()
     for index, l in enumerate(reader):
-        if param_print_lines:
-            print(f'Printing line {index}: {l}')
+        #if param_print_lines:
+            #print(f'Printing line {index}: {l}')
         l['row_number'] = index
         writer.writerow(l)
+        print('hotovo')
